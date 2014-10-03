9d0a15d047e828fe483b6cbc087efa792a45db29
David Matejka <matej21@matej21.cz>
2014-10-03 21:11:12 +0200

    Alternative event name completion for php 5.5 [WIP]

--- src/cz/juzna/intellij/kdyby/events/CompletionContributor.java
+++ src/cz/juzna/intellij/kdyby/events/CompletionContributor.java
@@ -3,14 +3,22 @@ package cz.juzna.intellij.kdyby.events;
 
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.completion.impl.CamelHumpMatcher;
+import com.intellij.codeInsight.lookup.AutoCompletionPolicy;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.project.Project;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.php.PhpIndex;
-import com.jetbrains.php.lang.parser.PhpElementTypes;
-import com.jetbrains.php.lang.psi.PhpPsiElementFactory;
-import com.jetbrains.php.lang.psi.elements.*;
+import com.jetbrains.php.completion.insert.PhpReferenceInsertHandler;
+import com.jetbrains.php.config.PhpLanguageFeature;
+import com.jetbrains.php.config.PhpLanguageLevel;
+import com.jetbrains.php.config.PhpProjectConfigurationFacade;
+import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression;
+import com.jetbrains.php.lang.psi.elements.Field;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -23,10 +31,12 @@ public class CompletionContributor extends com.intellij.codeInsight.completion.C
 				PlatformPatterns.psiElement().withParent(StringLiteralExpression.class),
 				new EventsCompletionProvider()
 		);
+
 		extend(CompletionType.BASIC,
-				PlatformPatterns.psiElement(),
+				PlatformPatterns.psiElement().inside(ArrayCreationExpression.class)
+						.andNot(PlatformPatterns.psiElement().withParent(StringLiteralExpression.class)),
 				new Php55EventsCompletionProvider()
-				);
+		);
 	}
 
 	private class EventsCompletionProvider extends CompletionProvider {
@@ -70,19 +80,45 @@ public class CompletionContributor extends com.intellij.codeInsight.completion.C
 	private class Php55EventsCompletionProvider extends CompletionProvider {
 		@Override
 		protected void addCompletions(@NotNull CompletionParameters completionParameters, ProcessingContext processingContext, @NotNull CompletionResultSet resultSet) {
-			PsiElement originalPosition = completionParameters.getOriginalPosition();
-			PhpIndex index = PhpIndex.getInstance(completionParameters.getPosition().getProject());
+			PsiElement position = completionParameters.getOriginalPosition();
+			if (position == null) {
+				return;
+			}
+			Project project = position.getProject();
+			PhpLanguageLevel languageLevel = PhpProjectConfigurationFacade.getInstance(project).getLanguageLevel();
+			if (!languageLevel.hasFeature(PhpLanguageFeature.CLASS_NAME_CONST)) {
+				return;
+			}
+			if (!EventsUtil.isInGetSubscribedEvents(position)) {
+				return;
+			}
+			PhpIndex index = PhpIndex.getInstance(project);
 			Collection<PhpClass> classes;
 			classes = new ArrayList<PhpClass>();
-			for (String className : index.getAllClassNames(new CamelHumpMatcher(""))) {
+			for (String className : index.getAllClassNames(resultSet.getPrefixMatcher())) {
 				classes.addAll(index.getClassesByName(className));
 			}
 
 			for (NetteEvent event : NetteEventUtil.findEvents(classes)) {
-				LookupElementBuilder lookupElementBuilder = LookupElementBuilder.create(event.className + "::class . '::" + event.getShortName() + "'")
-						.withPresentableText(event.getIdentifier());
-				resultSet.addElement(lookupElementBuilder.withIcon(Icons.EVENT_ICON));
+				Field field = event.resolveDeclaration(project);
+				if (field.getContainingClass() == null) {
+					continue;
+				}
+				LookupElement lookupElement = createLookupElement(event, field.getContainingClass());
+				resultSet.addElement(lookupElement);
 			}
 		}
 	}
+
+	private LookupElement createLookupElement(@NotNull NetteEvent event, @NotNull PhpClass phpClass) {
+		String shortName = event.className.substring(event.className.lastIndexOf("\\") + 1);
+		String lookupString = shortName + "::class . '::" + event.getShortName() + "'";
+		return LookupElementBuilder.create(phpClass, lookupString)
+				.withInsertHandler(PhpReferenceInsertHandler.getInstance())
+				.withPresentableText(event.getIdentifier())
+				.withIcon(Icons.EVENT_ICON)
+				.withAutoCompletionPolicy(AutoCompletionPolicy.NEVER_AUTOCOMPLETE);
+	}
+
+
 }
